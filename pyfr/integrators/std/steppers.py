beb8e07ed99547ef3a4e0031967944627b9e4a86
tarikdzanic <41964501+tarikdzanic@users.noreply.github.com>
2023-03-11 11:36:35 -0700

    Re-introduce explicit preproc/postproc calls

--- pyfr/integrators/std/steppers.py
+++ pyfr/integrators/std/steppers.py
@@ -21,11 +21,14 @@ class StdEulerStepper(BaseStdStepper):
         return self.nsteps
 
     def step(self, t, dt):
-        add, rhs_with_postproc = self._add, self.system.rhs
+        add, rhs = self._add, self.system.rhs
+        preproc, postproc = self.system.preproc, self.system.postproc
         ut, f = self._regidx
 
-        rhs_with_postproc(t, ut, f)
+        preproc(t, ut)
+        rhs(t, ut, f)
         add(1.0, ut, dt, f)
+        postproc(ut)
 
         return ut
 
@@ -41,7 +44,8 @@ class StdTVDRK3Stepper(BaseStdStepper):
         return 3*self.nsteps
 
     def step(self, t, dt):
-        add, rhs_with_postproc = self._add, self.system.rhs
+        add, rhs = self._add, self.system.rhs
+        preproc, postproc = self.system.preproc, self.system.postproc
 
         # Get the bank indices for each register (n, n+1, rhs)
         r0, r1, r2 = self._regidx
@@ -51,17 +55,23 @@ class StdTVDRK3Stepper(BaseStdStepper):
             r0, r1 = r1, r0
 
         # First stage; r2 = -∇·f(r0); r1 = r0 + dt*r2
-        rhs_with_postproc(t, r0, r2)
+        preproc(t, r0)
+        rhs(t, r0, r2)
         add(0.0, r1, 1.0, r0, dt, r2)
+        postproc(r1)
 
         # Second stage; r2 = -∇·f(r1); r1 = 0.75*r0 + 0.25*r1 + 0.25*dt*r2
-        rhs_with_postproc(t + dt, r1, r2)
+        preproc(t, r1)
+        rhs(t + dt, r1, r2)
         add(0.25, r1, 0.75, r0, 0.25*dt, r2)
+        postproc(r1)
 
         # Third stage; r2 = -∇·f(r1);
         #              r1 = 1.0/3.0*r0 + 2.0/3.0*r1 + 2.0/3.0*dt*r2
-        rhs_with_postproc(t + 0.5*dt, r1, r2)
+        preproc(t, r1)
+        rhs(t + 0.5*dt, r1, r2)
         add(2.0/3.0, r1, 1.0/3.0, r0, 2.0/3.0*dt, r2)
+        postproc(r1)
 
         # Return the index of the bank containing u(t + dt)
         return r1
@@ -78,7 +88,8 @@ class StdRK4Stepper(BaseStdStepper):
         return 4*self.nsteps
 
     def step(self, t, dt):
-        add, rhs_with_postproc = self._add, self.system.rhs
+        add, rhs = self._add, self.system.rhs
+        preproc, postproc = self.system.preproc, self.system.postproc
 
         # Get the bank indices for each register
         r0, r1, r2 = self._regidx
@@ -88,11 +99,14 @@ class StdRK4Stepper(BaseStdStepper):
             r0, r1 = r1, r0
 
         # First stage; r1 = -∇·f(r0)
-        rhs_with_postproc(t, r0, r1)
+        preproc(t, r0)
+        rhs(t, r0, r1)
 
         # Second stage; r2 = r0 + dt/2*r1; r2 = -∇·f(r2)
         add(0.0, r2, 1.0, r0, dt/2.0, r1)
-        rhs_with_postproc(t + dt/2.0, r2, r2)
+        postproc(r2)
+        preproc(t, r2)
+        rhs(t + dt/2.0, r2, r2)
 
         # As no subsequent stages depend on the first stage we can
         # reuse its register to start accumulating the solution with
@@ -103,7 +117,9 @@ class StdRK4Stepper(BaseStdStepper):
         # r2 = r0 + dt/2*r2
         # r2 = -∇·f(r2)
         add(dt/2.0, r2, 1.0, r0)
-        rhs_with_postproc(t + dt/2.0, r2, r2)
+        postproc(r2)
+        preproc(t, r2)
+        rhs(t + dt/2.0, r2, r2)
 
         # Accumulate; r1 = r1 + dt/3*r2
         add(1.0, r1, dt/3.0, r2)
@@ -112,10 +128,13 @@ class StdRK4Stepper(BaseStdStepper):
         # r2 = r0 + dt*r2
         # r2 = -∇·f(r2)
         add(dt, r2, 1.0, r0)
-        rhs_with_postproc(t + dt, r2, r2)
+        postproc(r2)
+        preproc(t, r2)
+        rhs(t + dt, r2, r2)
 
         # Final accumulation r1 = r1 + dt/6*r2 = u(t + dt)
         add(1.0, r1, dt/6.0, r2)
+        postproc(r1)
 
         # Return the index of the bank containing u(t + dt)
         return r1
