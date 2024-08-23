3b224da10379aefd5420d6c866b3a088a5e217b1
tarikdzanic <41964501+tarikdzanic@users.noreply.github.com>
2024-08-23 10:45:06 -0700

    Bugfix in linear system solver for BCs.

--- pyfr/solvers/bgk/inters.py
+++ pyfr/solvers/bgk/inters.py
@@ -78,9 +78,13 @@ class BGKBaseBCInters(BaseAdvectionBCInters):
         # Get reflections for wall BCs
         reflidxs = reflect(self.cfg, self.ndims)
 
+        # Get linear system size for DVM
+        N = self.ndims + 2
+
         tplargs = dict(ndims=self.ndims, nvars=self.nvars, nuvars=self.nuvars,
                        c=self.c, u=self.u, bctype=self.type, niters=self.niters,
-                       rsolver=rsolver, pi=np.pi, delta=delta, Pr=Pr, reflidxs=reflidxs)
+                       rsolver=rsolver, pi=np.pi, delta=delta, Pr=Pr,
+                       reflidxs=reflidxs, N=N)
         
         self.kernels['comm_flux'] = lambda: self._be.kernel(
             'bccflux', tplargs=tplargs, dims=[self.ninterfpts],
